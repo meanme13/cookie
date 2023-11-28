1f3519f60ccebcf7d1d0d1ac9cd3bd8cbc8d6c24
meanme13 <5656xv@gmail.com>
2023-11-29 00:30:29 +0200

    5th commit

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 
 let openModal = document.getElementById("openModal");
 let modalWindow = document.getElementById("modalWindow");
-let closeModal = document.getElementById("closeModal")
+let closeModal = document.getElementById("closeModal");
+let textInput = document.getElementById("textInput");
+let counter = document.getElementById("counter");
 
 openModal.addEventListener('click', () => {
-    modalWindow.style.display = 'block';
+    modalWindow.style.display = 'flex';
 });
 
 closeModal.addEventListener('click', () => {
     modalWindow.style.display = 'none';
+});
+
+textInput.addEventListener('input', () => {
+    let charCount = textInput.value.length;
+    counter.innerText = `( ${charCount} / 200 )`;
 });
